27d9c98ef1544bcff208ca48b89c2e1b5e7b7a1f
Yubor Molina <yubito.molina@gmail.com>
2022-04-17 21:07:47 -0500

    Deployed Website Test 4

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -8,8 +8,8 @@ const App = () =>{
     return(
         <div className='global-container' >
             <Routes>
-                <Route path='http://yubito98.github.io/yubor-molina' exact element={<Home/>}/>
-                <Route path='http://yubito98.github.io/yubor-molina'exact element={<AboutMe/>}/>
+                <Route path='/yubor-molina' exact element={<Home/>}/>
+                <Route path='/yubor-molina'exact element={<AboutMe/>}/>
                 <Route element={<NotFound/>} />
             </Routes>
         </div>
