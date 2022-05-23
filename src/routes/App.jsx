6707f948614b2d7bbba4e7436b4213e31ed28fc8
Yubor Molina <yubor98@gmail.com>
2022-05-22 22:08:28 -0500

    solving routing

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Routes, Route} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from '../pages/Home';
 import AboutMe from '../pages/AboutMe';
 
 const App = () =>{
     return(
-        <div className='global-container' >
+        <BrowserRouter>
             <Routes>
                 <Route path='/yubor-molina' element={<Home/>}/>
                 <Route path='/about-me' element={<AboutMe/>}/>
             </Routes>
-        </div>
+       </BrowserRouter>
+      
     )
 }
 
