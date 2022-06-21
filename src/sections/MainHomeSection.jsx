b36a4b3fe74fd98ae2d7c221357bdeee706a52bb
Yubor Molina <yubor98@gmail.com>
2022-06-20 22:01:12 -0500

    background for hero section

--- src/sections/MainHomeSection.jsx
+++ src/sections/MainHomeSection.jsx
@@ -5,18 +5,22 @@ import SquareAnimation from "../components/SquareAnimation";
 import '../styles/sections-styles/MainHomeSection.css'
 import yubor from '../assets/yubor.jpg'
 
+
 const MainHomeSection = () =>{
     return(
-        <div className="main-home-section custom-container">
-            <MainCard 
-                subtitle="What's up! This is"
-                title="Yubor Molina"
-                skills="React"
-                text="I am a Colombian front-end web developer with experience using technologies such as React.js, Next.js, and the HubSpot CMS. I am currently working for a company that helps the biggest HubSpot partner agencies around the world to tier up and scale fast."
-                image={yubor}
-             />
-             <TriangleAnimation/>
-             <SquareAnimation/>
+        <div className="main-home-section">
+            <div class="custom-container">
+                <MainCard 
+                    subtitle="What's up! This is"
+                    title="Yubor Molina"
+                    skills="React"
+                    text="I am a Colombian front-end web developer with experience using technologies such as React.js, Next.js, and the HubSpot CMS. I am currently working for a company that helps the biggest HubSpot partner agencies around the world to tier up and scale fast."
+                    image={yubor}
+                />
+                <TriangleAnimation/>
+                <SquareAnimation/>
+            </div>
+            
         </div>
     )
 }
